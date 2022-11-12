a54017fc2f2976407517fdc95b57cdc615d5717d
unknown <biplob2358@gmail.com>
2022-11-12 13:11:11 +0600

    banner added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Banner from "../Banner/Banner";
+import InfoCards from "../InfoCards/InfoCards";
 
 const Home = () => {
   return (
     <div className="mx-5">
-      <h2>This is home</h2>
+      <Banner></Banner>
+      <InfoCards></InfoCards>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+
+          secondary: "#19D3AE",
+
+          accent: "#3A4256",
+
+          neutral: "#191D24",
+
+          "base-100": "#f3f4f6",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
